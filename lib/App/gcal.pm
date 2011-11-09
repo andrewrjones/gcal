240b3259fff8829eaf0f2141846cc79bf07d59ad
Andrew Jones <andrew.jones@arm.com>
2011-11-09 13:38:23 +0000

    initial checkin. WIP

---
+++ lib/App/gcal.pm
@@ -0,0 +1,20 @@
+use strict;
+use warnings;
+
+package App::gcal;
+
+# ABSTRACT: Command Line Interface interface to Google Calendar.
+
+sub run {
+    my @args = @_;
+}
+
+=head1 DESCRIPTION
+
+The C<gcal> command provides a quick and easy interface to Google Calendar from the command line.
+
+=head1 TODO
+
+=cut
+
+1;
