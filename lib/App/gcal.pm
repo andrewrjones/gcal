db15b8764af8f5ba14110e7083402d9ffe595b4e
Andrew Jones <andrew.jones@arm.com>
2011-11-10 14:23:51 +0000

    implement _process_file and start implementing _save_to_gcal

--- lib/App/gcal.pm
+++ lib/App/gcal.pm
@@ -3,18 +3,104 @@ use warnings;
 
 package App::gcal;
 
+use Class::ReturnValue;
+use Data::ICal;
+use Net::Google::Calendar;
+use Net::Google::Calendar::Entry;
+use DateTime::Format::ICal;
+
+use Smart::Comments;
+
 # ABSTRACT: Command Line Interface interface to Google Calendar.
 
+our $gcal;
+
 sub run {
-    my @args = @_;
-    
+    my (@args) = @_;
+
     # loop over args
-    
-    # if file, open and parse with Data::ICal
-    
-    # if not, try and parse with ICal::QuickAdd
-    
-    # if success, use Net::Google::Calendar to save
+    for my $arg (@args) {
+
+        my $cal;
+        if ( ( -e $arg ) && ( -r $arg ) ) {
+
+            # looks like a file
+            $cal = _process_file($arg);
+
+        }
+        else {
+
+            # give to ICal::QuickAdd
+            $cal = _process_text($arg);
+        }
+
+        if ($cal) {
+            _save_to_gcal($cal);
+        }
+        else {
+            print STDERR $cal->error_message . "\n";
+        }
+    }
+}
+
+sub _process_file {
+    my ($file) = @_;
+
+    my $calendar = Data::ICal->new( filename => $file );
+    unless ($calendar) {
+        return _error("error parsing $file");
+    }
+
+    return $calendar;
+}
+
+sub _process_text {
+    my ($text) = @_;
+
+    # TODO
+}
+
+sub _save_to_gcal {
+    my ($cal) = @_;
+
+    unless ($gcal) {
+        $gcal = Net::Google::Calendar->new;
+
+        # TODO: load from .netrc, or similar
+        $gcal->login( '', '' );
+    }
+
+    for my $entry ( @{ $cal->entries } ) {
+
+        # create gcal event
+        # TODO: extract & test
+        my $event = Net::Google::Calendar::Entry->new();
+
+        $event->title( $entry->property('summary')->[0]->value );
+        $event->content( $entry->property('description')->[0]->value );
+        $event->location( $entry->property('location')->[0]->value );
+        $event->when(
+            DateTime::Format::ICal->parse_datetime(
+                $entry->property('dtstart')->[0]->value
+            ),
+            DateTime::Format::ICal->parse_datetime(
+                $entry->property('dtend')->[0]->value
+            )
+        );
+        $event->status('confirmed');
+
+        # save
+        my $tmp = $gcal->add_entry($event);
+        die "Couldn't add event: $@\n" unless defined $tmp;
+    }
+}
+
+sub _error {
+    my $msg = shift;
+
+    my $ret = Class::ReturnValue->new;
+    $ret->as_error( errno => 1, message => $msg );
+    return $ret;
 }
 
 =head1 DESCRIPTION
--- t/01.t
+++ t/01.t
@@ -3,8 +3,16 @@
 use strict;
 use warnings;
 
-use Test::More tests => 2;
+use Test::More tests => 5;
+use FindBin qw($Bin);
 
 BEGIN { use_ok('App::gcal'); }
 require_ok('App::gcal');
 
+my $err_from_ics = App::gcal::_process_file("$Bin/../dist.ini");
+isa_ok( $err_from_ics, 'Class::ReturnValue' );
+like( $err_from_ics->error_message, qr/error parsing/ );
+
+my $testfile_simple = "$Bin/resources/simple.ics";    # version 2, one event
+my $cal_from_ics = App::gcal::_process_file($testfile_simple);
+isa_ok( $cal_from_ics, 'Data::ICal' );
